3d1d1a91178ace2ebfdaba34ae4e0781f2966ad2
Christopher-Grell <cgrell2@huskers.unl.edu>
2020-08-20 16:51:18 -0500

    Initial commit

---
+++ Hello/Hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Christopher Grell
+ *
+ * Date: 2020/03/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Christopher Grell\n");
+
+  return 0;
+}
